97e1768f2dbaa28b2d2965837c91be61cfae20ff
Lizzy <lizzy.guanli@gmail.com>
2018-05-25 18:49:58 -0500

    No errors so far!

---
+++ Beauty/ProductViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ProductViewController.swift
+//  Beauty
+//
+//  Created by Lizzy on 2018/5/25.
+//  Copyright © 2018年 L&L. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var nameText: UITextField!
+    @IBOutlet var brandText: UITextField!
+    @IBOutlet var categoryText: UITextField!
+    @IBOutlet var produceDateText: UITextField!
+    @IBOutlet var openSwitch: UISwitch!
+    @IBOutlet var effectText: UITextField!
+    @IBOutlet var openDateText: UITextField!
+    @IBOutlet var expireDateText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func doneTapped(_ sender: Any) {
+    }
+    
+    
+
+}
